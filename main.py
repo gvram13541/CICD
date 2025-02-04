42caab9cddf4de1e76946a0c49548dfeff47e7a8
Perugu Gunavardhan Reddy <perugugunavardhanreddy@Perugus-MacBook-Air.local>
2025-02-05 02:51:10 +0530

    fc

---
+++ main.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    return a + b
+
+if __name__ == '__main__':
+    a = int(input("Enter first number: "))  # Adding a prompt for clarity
+    b = int(input("Enter second number: "))
+    print(add(a, b))
